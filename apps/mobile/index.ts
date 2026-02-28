20a21ea1905604775ab2547bf52006b961dbd4b9
KingofSalem33 <107441786+KingofSalem33@users.noreply.github.com>
2026-02-28 07:34:28 -0600

    Add env-gated mobile crash instrumentation (#32)

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -1,9 +1,13 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
+import * as Sentry from "@sentry/react-native";
 
 import App from "./App";
+import { initMobileMonitoring } from "./src/lib/monitoring";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+initMobileMonitoring();
+
+registerRootComponent(Sentry.wrap(App));
