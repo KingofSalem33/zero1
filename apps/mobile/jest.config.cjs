2658b7ee6613a90e948afc6bd5ecf2a33db1343c
KingofSalem33 <huskiesniu26@gmail.com>
2026-02-28 00:22:19 -0600

    Fix mobile Jest React path mapping for CI

--- apps/mobile/jest.config.cjs
+++ apps/mobile/jest.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
   testMatch: ["<rootDir>/src/**/__tests__/**/*.test.ts?(x)"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    "^react$": "<rootDir>/node_modules/react",
-    "^react-dom$": "<rootDir>/node_modules/react-dom",
-    "^react-test-renderer$": "<rootDir>/node_modules/react-test-renderer",
+    "^react$": "<rootDir>/../../node_modules/react",
+    "^react-dom$": "<rootDir>/../../node_modules/react-dom",
+    "^react-test-renderer$": "<rootDir>/../../node_modules/react-test-renderer",
   },
 };
